5866f3c3f1641370a9f5e63c0e2164fde04bc515
Mirlan <m.maksitaliev@gmail.com>
2018-10-09 19:02:34 +0600

    routes and pages

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,29 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Header from "../Header";
 import Sidebar from "../Sidebar";
+import Content from "../Content";
+import Collection from "../pages/Collection";
+import Trending from "../pages/Trending";
+import Search from "../pages/Search";
 
 class App extends Component {
   render() {
     return (
-      <div className="app">
-        <Header />
-        <Sidebar />
-      </div>
+      <Router>
+        <div className="app">
+          <Header />
+          <Sidebar />
+          <Switch>
+            <Route exact path="/" component={Content} />
+            <Route exact path="/collection" component={Collection} />
+            <Route exact path="/trending" component={Trending} />
+            <Route exact path="/search" component={Search} />
+            <Route render={() => <h1>Page Not Found</h1>} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
-export default (props) => {
+export default props => {
   return (
     <div className="sidebar">
-      Sidebar
+      <ul className="sidebar__list">
+        <li className="sidebar__list-item">
+          <Link to="/collection" className="sidebar__link">
+            My Collection
+          </Link>
+        </li>
+        <li className="sidebar__list-item">
+          <Link to="/trending" className="sidebar__link">
+            Trending Movies
+          </Link>
+        </li>
+        <li className="sidebar__list-item">
+          <Link to="/search" className="sidebar__link">
+            History
+          </Link>
+        </li>
+      </ul>
     </div>
-  )
-}
+  );
+};
