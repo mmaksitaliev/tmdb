04a7fa9168959bfa524d9753327eb6ffee4d7a80
Mirlan <m.maksitaliev@gmail.com>
2018-10-09 20:47:54 +0600

    added movie component, utils.scss

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,10 +1,16 @@
-import React from 'react'
-import Card from "../Card"
+import React from "react";
+import Card from "../Card";
 
-export default (props) => {
+export default props => {
   return (
-    <Card>
-
-    </Card>
-  )
-}
+    <div>
+      <Card>
+        <img src="" alt="movie image" className="movie__image" />
+      </Card>
+      <div className="movie__details">
+        <h4 className="movie__title mb-1">Fight club</h4>
+        <h4 className="movie__year">1999</h4>
+      </div>
+    </div>
+  );
+};
