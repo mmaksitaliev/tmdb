7eb0469a1ba0b2f7905a46453e50f072be11e816
Mirlan <m.maksitaliev@gmail.com>
2018-10-10 20:17:40 +0600

    configured store for movie details

---
+++ src/store/reducers/details.js
@@ -0,0 +1,29 @@
+import { createReducers } from "redux-arc";
+import { movie } from "../actions";
+
+const INITIAL_STATE = { details: null, loading: false, error: null };
+
+const onRequest = (state = INITIAL_STATE, action) => {
+  return {
+    ...state,
+    loading: true,
+    details: INITIAL_STATE.details,
+    error: INITIAL_STATE.error
+  };
+};
+
+const onResponse = (state = INITIAL_STATE, { payload }) => {
+  return {
+    ...state,
+    details: payload,
+    loading: true,
+    error: INITIAL_STATE.error
+  };
+};
+
+const HANDLERS = {
+  [movie.types.LOAD_DETAILS.REQUEST]: onRequest,
+  [movie.types.LOAD_DETAILS.RESPONSE]: onResponse
+};
+
+export default createReducers(INITIAL_STATE, HANDLERS);
