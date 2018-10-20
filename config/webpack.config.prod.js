76599e67889e8587676ea23af33ccfe7c7107946
Mirlan <m.maksitaliev@gmail.com>
2018-10-20 20:51:48 +0600

    testing setup complete

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -191,6 +191,11 @@ module.exports = {
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
       'react-native': 'react-native-web',
+
+      components: path.resolve(__dirname, '../src/components/'),
+      containers: path.resolve(__dirname, '../src/containers/'),
+      pages: path.resolve(__dirname, '../src/pages/'),
+      store: path.resolve(__dirname, '../src/store/'),
     },
     plugins: [
       // Adds support for installing with Plug'n'Play, leading to faster installs and adding
